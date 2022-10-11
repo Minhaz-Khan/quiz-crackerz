bd42f6cdf5c93ca8e8e7c576c49a79190c532f6b
Minhaz-Khan <kminhaz111@gmail.com>
2022-10-12 04:08:11 +0600

    total marks count done

--- src/Component/Option/Option.js
+++ src/Component/Option/Option.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Markscontext } from '../Quastions/Quastions';
 
 const Option = ({ option, correctAnswer }) => {
+    const handleQuizMarks = useContext(Markscontext);
     const notify = (isCorrect) => toast(isCorrect);
-    const [countAns, setCountAns] = useState(0);
-    const [color, setColor] = useState(false);
+    const [color, setColor] = useState('');
     const checkingRightAns = (Option) => {
         if (Option) {
+            setColor('')
             if (Option === correctAnswer) {
                 notify('Correct Anwser')
                 setColor(!color)
+                handleQuizMarks(true)
+                setColor('true')
             }
             else {
                 notify('Wrong Answer')
+                handleQuizMarks(!true)
+                setColor('false')
+
             }
         }
     }
 
     return (
-        <div className='cursor-pointer border-2 h-24 flex justify-center items-center' onClick={() => checkingRightAns(option)}>
+        <div className={`cursor-pointer border-2 h-24 flex justify-center items-center ${color === 'true' && 'text-green-500 border-green-600'
+            }`} onClick={() => checkingRightAns(option)}>
             <p className='text-lg'>{option}</p>
             <ToastContainer></ToastContainer>
 
--- src/Component/Quastions/Quastions.js
+++ src/Component/Quastions/Quastions.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
 import Quiz from '../Quiz/Quiz';
+export const Markscontext = createContext('hello')
 
 const Quastions = () => {
     const { data } = useLoaderData();
     const { questions, name } = data;
-
+    const [correct, setCorrec] = useState(0);
+    const [wrong, setWrong] = useState(0);
+    const handleQuizMarks = (isCorrect) => {
+        if (isCorrect) {
+            setCorrec(correct + 1);
+        }
+        else {
+            setWrong(wrong + 1)
+        }
+    }
     return (
-        <div className='my-10 '>
-            <ToastContainer />
-            <h1 className='text-3xl text-center mb-10'>Quiz of {name}</h1>
-            <div className='grid grid-cols-1 mx-auto gap-10 lg:w-6/12 w-full'>
-                {questions.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)}
+        <Markscontext.Provider value={handleQuizMarks}>
+            <div className='my-10 grid grid-cols-12'>
+                <div className='col-span-9'>
+                    <h1 className='text-3xl text-center mb-10'>Quiz of {name}</h1>
+                    <div className='grid grid-cols-1 mx-auto gap-10 lg:w-6/12 w-full'>
+                        {questions.map(quiz => <Quiz key={quiz.id} quiz={quiz}></Quiz>)}
+                    </div>
+                </div>
+                <div className='col-span-3 h-96 bg-slate-200 p-10'>
+                    <div className='my-auto'>
+                        <h3 className='text-3xl '>Correct Answer:{correct}</h3>
+                        <h3 className='text-3xl '>Wrong Answer:{wrong}</h3>
+                    </div>
+                </div>
             </div>
-        </div>
+        </Markscontext.Provider>
     );
 };
 
